3489b96669b99eff8633c8481ebe098fe2c61917
Erika Hairston <erikahairston@gmail.com>
2016-11-10 12:35:11 -0500

    step 3 retrieval

--- register.py
+++ register.py
@@ -6,8 +6,10 @@ import sys
 import re
 import requests
 import json
+from pprint import pprint
 
 
+######Receiving Enpoints#######
 #####Step 1#####
 endpoint1 = "http://challenge.code2040.org/api/register"
 
@@ -16,31 +18,41 @@ endpoint1 = "http://challenge.code2040.org/api/register"
 endpoint2 = "http://challenge.code2040.org/api/reverse"
 endpoint2_5 = "http://challenge.code2040.org/api/reverse/validate"
 
+
+#####Step 3#######
+endpoint3 = "http://challenge.code2040.org/api/haystack"
+
+############################################################
+
+
+
+#####conent sent to endoint########
 headers = {'Content-Type' : 'application/json'}
 body = json.dumps({'token': '60003627513e0d6ab250539770e1d9ed', 'github': 'http://github.com/erikahairston/code2040Challenge'})
 
-response = requests.post(endpoint2, data=body, headers = headers)
+response = requests.post(endpoint3, data=body, headers = headers)
 # Make the POST request here, passing body as the data:
 
-print(response.status_code)
-print(response.content)
+diction = json.loads(response.content)
 
-print("reverse")
+print(response.status_code)
+pprint(diction)
 
 ##Step 2_5 post#####
-reversedS = response.content[::-1]
-body2 = json.dumps({'token': '60003627513e0d6ab250539770e1d9ed', 'string': reversedS})
+# reversedS = response.content[::-1]
+# body2 = json.dumps({'token': '60003627513e0d6ab250539770e1d9ed', 'string': reversedS})
 
-final = requests.post(endpoint2_5, data=body2, headers = headers)
+# final = requests.post(endpoint2_5, data=body2, headers = headers)
 
-print(final.content)
+# print(final.content)
 
-print("reverse")
-print(response.status_code)
-print(response.content[::-1])
+# print("reverse")
+# print(response.status_code)
+
+# print(response.content[::-1])
 
 
-
+##Step 3 post#####
 
 
 
