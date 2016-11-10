2797249b579517a71e7c033b0afa8935e2304b64
Erika Hairston <erikahairston@gmail.com>
2016-11-10 02:41:03 -0500

    added Content-type,

--- register.py
+++ register.py
@@ -3,10 +3,14 @@
 # Host: www.codecademy.com
 # Content-Type: text/html; charset=UTF-8
 # Name=Eric&Age=26
-
+import sys
+import re
 import requests
 
+headers = {'Content-Type' : 'application/json'}
 body = {'token': '60003627513e0d6ab250539770e1d9ed', 'github': 'https://github.com/erikahairston/code2040Challenge'}
 
-response = requests.post("http://challenge.code2040.org/api/register", data=body)
+response = requests.post("http://challenge.code2040.org/api/register", data=body, headers = headers)
 # Make the POST request here, passing body as the data:
+
+print(response.status_code)
