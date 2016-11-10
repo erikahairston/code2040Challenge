cf259c006ff46d83f5bd3a2ebde998ba6ab43213
Erika Hairston <erikahairston@gmail.com>
2016-11-10 03:02:48 -0500

    encode JSON
    fixed error code to encode JSON
    
    body = json.dumps({'token': '60003627513e0d6ab250539770e1d9ed',
    'github': 'http://github.com/erikahairston/code2040Challenge'})

--- register.py
+++ register.py
@@ -1,16 +1,17 @@
-########## Example request #############
-# POST /learn-http HTTP/1.1
-# Host: www.codecademy.com
-# Content-Type: text/html; charset=UTF-8
-# Name=Eric&Age=26
+########## CODE2040 Registration #############
+# POST request
+# Erika A. Hairston 11/1/16
+
 import sys
 import re
 import requests
+import json
 
 headers = {'Content-Type' : 'application/json'}
-body = {'token': '60003627513e0d6ab250539770e1d9ed', 'github': 'https://github.com/erikahairston/code2040Challenge'}
+body = json.dumps({'token': '60003627513e0d6ab250539770e1d9ed', 'github': 'http://github.com/erikahairston/code2040Challenge'})
 
 response = requests.post("http://challenge.code2040.org/api/register", data=body, headers = headers)
 # Make the POST request here, passing body as the data:
 
 print(response.status_code)
+print(response)
