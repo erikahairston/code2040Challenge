67f07c18d24ac99e500b6f9f4349e18e5e1a570a
Erika Hairston <erikahairston@gmail.com>
2016-11-10 02:16:15 -0500

    githubURL
    https://github.com/erikahairston/code2040Challenge

--- register.py
+++ register.py
@@ -6,7 +6,7 @@
 
 import requests
 
-body = {'token': '60003627513e0d6ab250539770e1d9ed', 'github': '26'}
+body = {'token': '60003627513e0d6ab250539770e1d9ed', 'github': 'https://github.com/erikahairston/code2040Challenge'}
 
 response = requests.post("http://challenge.code2040.org/api/register", data=body)
 # Make the POST request here, passing body as the data:
