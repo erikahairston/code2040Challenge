736a9e7a452c2d1419ebaa6cbbfb6855d50680fb
Erika Hairston <erikahairston@gmail.com>
2016-11-10 02:14:40 -0500

    POST request
    submitted POST request

---
+++ register.py
@@ -0,0 +1,12 @@
+########## Example request #############
+# POST /learn-http HTTP/1.1
+# Host: www.codecademy.com
+# Content-Type: text/html; charset=UTF-8
+# Name=Eric&Age=26
+
+import requests
+
+body = {'token': '60003627513e0d6ab250539770e1d9ed', 'github': '26'}
+
+response = requests.post("http://challenge.code2040.org/api/register", data=body)
+# Make the POST request here, passing body as the data:
