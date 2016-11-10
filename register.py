d5d12ae479636ae8f0edc856b3eda8458aa9d0b3
Erika Hairston <erikahairston@gmail.com>
2016-11-10 03:39:10 -0500

    step2
    revers string
    post to endpoint

--- register.py
+++ register.py
@@ -7,11 +7,40 @@ import re
 import requests
 import json
 
+
+#####Step 1#####
+endpoint1 = "http://challenge.code2040.org/api/register"
+
+
+####Step 2#####
+endpoint2 = "http://challenge.code2040.org/api/reverse"
+endpoint2_5 = "http://challenge.code2040.org/api/reverse/validate"
+
 headers = {'Content-Type' : 'application/json'}
 body = json.dumps({'token': '60003627513e0d6ab250539770e1d9ed', 'github': 'http://github.com/erikahairston/code2040Challenge'})
 
-response = requests.post("http://challenge.code2040.org/api/register", data=body, headers = headers)
+response = requests.post(endpoint2, data=body, headers = headers)
 # Make the POST request here, passing body as the data:
 
 print(response.status_code)
 print(response.content)
+
+print("reverse")
+
+##Step 2_5 post#####
+reversedS = response.content[::-1]
+body2 = json.dumps({'token': '60003627513e0d6ab250539770e1d9ed', 'string': reversedS})
+
+final = requests.post(endpoint2_5, data=body2, headers = headers)
+
+print(final.content)
+
+print("reverse")
+print(response.status_code)
+print(response.content[::-1])
+
+
+
+
+
+
